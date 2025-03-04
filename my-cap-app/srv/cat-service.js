3eb108cba0fef1b8d575c879aac82be073b1eabd
GabrieleMurgia <gabriele.murgia.gm@gmail.com>
2025-03-04 14:49:16 +0100

    First commit full stack app

--- my-cap-app/srv/cat-service.js
+++ my-cap-app/srv/cat-service.js
@@ -4,6 +4,7 @@ module.exports = cds.service.impl(async function() {
     const { Books } = this.entities;
 
     this.on('INSERT', Books, async (req) => {
+        debugger
         if (!req.data.title) {
             req.error(400, 'Title is required!!');
         }
